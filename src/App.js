1e10a7f83b6399348767bd1e9e17a4684eda2775
shameerkhan <shameerkhang@gmail.com>
2023-11-13 06:24:46 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <div className="wrapper">
+        <div className="control">
+          <input type="text" name="" id="" />
+          <input type="button" value="Update List" />
+        </div>
+        <div className="list">
+          <div className="item">
+            <span className="item__id">12</span>
+            <h4 className="item__title">Item Title</h4>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
