60e04f0e4634199b4626655b0f326795ac6317ce
shameerkhan <shameerkhang@gmail.com>
2023-11-22 08:25:15 +0000

    Update 2 files

--- src/Form.js
+++ src/Form.js
@@ -1,39 +1,37 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
+import Input from "./Components/Input";
 
 const Form = (props) => {
-  const taskTitleInput = useRef();
-  const taskApplicationInput = useRef();
-
   const updateData = props.updateTask;
+
+  const [task, setTask] = useState("");
+  const [application, setApplication] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    updateData({
-      id: "",
-      Task: taskTitleInput.current.value,
-      Application: taskApplicationInput.current.value,
-    });
+    console.log(task);
+    console.log(application);
   };
-
   return (
     <div className="control">
       <form onSubmit={handleSubmit}>
-        <label htmlFor="taskName" className="control-label">
-          Task Title:
-        </label>
-        <br />
-        <input type="text" id="taskName" ref={taskTitleInput} />
+        <Input
+          lable="Task title"
+          id="Task"
+          value={task}
+          onChange={(id, value) => {
+            setTask(value);
+          }}
+        />
         <br /> <br />
-        <label htmlFor="taskCat" className="control-label">
-          Task Category:
-        </label>
-        <br />
-        <input
-          type="text"
-          id="taskCat"
-          ref={taskApplicationInput}
-        /> <br /> <br />
-        <input type="submit" value="Update List" />
+        <Input
+          lable="Application"
+          id="Application"
+          value={application}
+          onChange={(id, value) => setApplication(value)}
+        />
+        <br /> <br />
+        <input type="submit" value="Update List" onClick={handleSubmit} />
       </form>
     </div>
   );
